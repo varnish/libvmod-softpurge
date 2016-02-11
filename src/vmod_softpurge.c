54bb8504944e15f883621985cc10881cdb276d86
Federico G. Schwindt <fgsch@lodoss.net>
2016-02-11 17:41:56 +0000

    Polish

--- src/vmod_softpurge.c
+++ src/vmod_softpurge.c
@@ -1,8 +1,8 @@
-#include <stdlib.h>
-
 #include "vrt.h"
+#include "vcl.h"
 #include "cache/cache.h"
 #include "hash/hash_slinger.h"
+
 #include "vcc_if.h"
 
 void
@@ -15,9 +15,9 @@ vmod_softpurge(const struct vrt_ctx *ctx)
 	double now;
 
 	CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
-
-	if (!ctx->req) {
-		VSL(SLT_Debug, 0, "softpurge() is only valid in vcl_hit and vcl_miss.");
+	if (ctx->method != VCL_MET_HIT && ctx->method != VCL_MET_MISS) {
+		VSLb(ctx->vsl, SLT_VCL_Error, "softpurge() is only "
+		    "available in vcl_hit{} and vcl_miss{}");
 		return;
 	}
 
@@ -27,35 +27,27 @@ vmod_softpurge(const struct vrt_ctx *ctx)
 		CHECK_OBJ_NOTNULL(ctx->req->obj->objcore, OBJCORE_MAGIC);
 		CHECK_OBJ_NOTNULL(ctx->req->obj->objcore->objhead, OBJHEAD_MAGIC);
 		oh = ctx->req->obj->objcore->objhead;
-		VSL(SLT_Debug, 0, "Softpurge running inside vcl_hit");
 	} else if (ctx->req->objcore != NULL) {
 		CHECK_OBJ_NOTNULL(ctx->req->objcore, OBJCORE_MAGIC);
 		CHECK_OBJ_NOTNULL(ctx->req->objcore->objhead, OBJHEAD_MAGIC);
 		oh = ctx->req->objcore->objhead;
-		VSL(SLT_Debug, 0, "Softpurge running inside vcl_miss");
-	} else {
-		VSL(SLT_Debug, 0, "softpurge() is only valid in vcl_hit and vcl_miss.");
-		return;
 	}
 
-	now = ctx->req->t_prev;
 	CHECK_OBJ_NOTNULL(oh, OBJHEAD_MAGIC);
-
 	spc = WS_Reserve(ctx->ws, 0);
-	ocp = (void*)ctx->ws->f;
+	assert(spc >= sizeof *ocp);
 
+	nobj = 0;
+	ocp = (void*)ctx->ws->f;
 	Lck_Lock(&oh->mtx);
 	assert(oh->refcnt > 0);
-	nobj = 0;
+	now = ctx->req->t_prev;
 	VTAILQ_FOREACH(oc, &oh->objcs, list) {
-	    	CHECK_OBJ_NOTNULL(oc, OBJCORE_MAGIC);
+		CHECK_OBJ_NOTNULL(oc, OBJCORE_MAGIC);
 		assert(oc->objhead == oh);
-		if (oc->flags & OC_F_BUSY) {
+		if (oc->flags & OC_F_BUSY)
 			continue;
-		}
-
 		(void)oc_getobj(&ctx->req->wrk->stats, oc);
-
 		xxxassert(spc >= sizeof *ocp);
 		oc->refcnt++;
 		spc -= sizeof *ocp;
@@ -67,29 +59,11 @@ vmod_softpurge(const struct vrt_ctx *ctx)
 		oc = ocp[n];
 		CHECK_OBJ_NOTNULL(oc, OBJCORE_MAGIC);
 		o = oc_getobj(&ctx->req->wrk->stats, oc);
-		if (o == NULL) {
+		if (o == NULL)
 			continue;
-		}
 		CHECK_OBJ_NOTNULL(o, OBJECT_MAGIC);
-
-		/*
-		   object really expires at o->exp.t_origin + o->exp.ttl, or at
-		   an earlier point for this request if overridden in TTL.
-		*/
-
-		VSL(SLT_Debug, 0, "XX: object BEFORE.  ttl ends in %.3f, grace ends in %.3f for object %i",
-		    (EXP_Ttl(ctx->req, o) - now),
-		    (EXP_Ttl(ctx->req, o) + o->exp.grace - now), n);
-
-		// Update the object's TTL so that it expires right now.
 		if (o->exp.ttl > (now - o->exp.t_origin))
 			EXP_Rearm(o, now, 0, o->exp.grace, o->exp.keep);
-
-		VSL(SLT_Debug, 0, "XX: object updated. ttl ends in %.3f, grace ends in %.3f for object %i",
-		    (EXP_Ttl(ctx->req, o) - now),
-		    (EXP_Ttl(ctx->req, o) + o->exp.grace - now), n);
-
-		// decrease refcounter and clean up if the object has been removed.
 		(void)HSH_DerefObj(&ctx->req->wrk->stats, &o);
 	}
 	WS_Release(ctx->ws, 0);
